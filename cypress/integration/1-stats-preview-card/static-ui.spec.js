30d583c4678c70453c03716551097efa45af1fc4
hans <hans.krebs@me.com>
2021-08-27 17:56:15 +0200

    Fix cypress test.

--- cypress/integration/1-stats-preview-card/static-ui.spec.js
+++ cypress/integration/1-stats-preview-card/static-ui.spec.js
@@ -1,6 +1,10 @@
 /// <reference types="Cypress" />
 
 describe('Static UI', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
     it('should render correctly', () => {
         cy.get('#title').should('have.text', 'Get insights that help your business grow.');
     });
